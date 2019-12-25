5cd9c4ce2d447e187b363ad04fb76405c30f06f5
johnnybluhm <54964159+johnnybluhm@users.noreply.github.com>
2019-12-24 19:22:13 -0700

    added webdriver wait
    program runs much faster now. also writes classes to file

---
+++ class_array.py
@@ -0,0 +1,25 @@
+courses  = open("degree_audit.txt").read()
+split=courses.split("\n")
+class_array=[]
+for course in split:
+    class_array.append(course.split(","))
+
+print(class_array)
+final_string=[]
+
+for courses in class_array:
+    subject=courses[0][0:4]
+    for index, course_number in enumerate(courses):
+        if(index==0):
+            course_name = subject+' '+courses[0][4:8]
+            final_string.append(course_name)
+            print(course_name)
+        else:
+            course_name=subject+' '+course_number
+            final_string.append(course_name)
+
+f = open("array_of_classes.txt", "w")
+f.write(str(final_string))
+f.close
+
+
--- web_scraper.py
+++ web_scraper.py
@@ -1,23 +1,50 @@
 from selenium import webdriver
-import time
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 from dateutil import parser
 import pandas as pd
 from datetime import datetime
 from pprint import pprint
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+
+
+courses  = open("degree_audit.txt").read()
+split=courses.split("\n")
+class_array=[]
+for course in split:
+    class_array.append(course.split(","))
+
+
+final_string=[]
+
+for courses in class_array:
+    subject=courses[0][0:4]
+    for index, course_number in enumerate(courses):
+        if(index==0):
+            course_name = subject+' '+courses[0][4:8]
+            final_string.append(course_name)
+            
+        else:
+            course_name=subject+' '+course_number
+            final_string.append(course_name)
 
 
 
 driver = webdriver.Chrome()
-driver.implicitly_wait(10)
+wait = WebDriverWait(driver, 1)
+
+
 
 #opens browser
 
 
 #hard code class values for now
 course_list = []
-course_sel = ['CSCI 2400', 'MATH 1150','ITAL 1020']
+course_sel = final_string
 
 #after for loop array with each element being an array of class objects is available
 for index,sel in enumerate(course_sel):
@@ -38,7 +65,7 @@ for index,sel in enumerate(course_sel):
     inputElement.submit()
 
     #give ajax time to laod
-    time.sleep(2)
+    wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "result__code")))
 
     getNodes = 'var results =document.getElementsByClassName("result__code");\n'
     loop = 'for(var i=0;i<results.length;i++){\n'
@@ -49,100 +76,101 @@ for index,sel in enumerate(course_sel):
     #search classes for selection
     driver.execute_script(js)
 
-
-
     #give ajax time to load
-    time.sleep(2)
+    #if timeout exception is thrown just continue loop
+    try:
+        table= wait.until(EC.presence_of_element_located((By.CLASS_NAME, "course-sections")))        
+    except:
+        table = None
+
+    #table = driver.execute_script('var a = document.getElementsByClassName("course-sections")[0];\n return a;')
+
+    if(table != None):
+    
+        table= table.get_attribute('outerHTML')
+        soup=BeautifulSoup(table, 'html.parser')
 
 
-    #gets table of course info for course
-    table = driver.execute_script('var a = document.getElementsByClassName("course-sections")[0];\n return a;').get_attribute('outerHTML')
+        #anchor tags have all course info, is an array
+        courses = soup.find_all("a")
 
+        #array to hold course dictionaries
+        course_array = []
 
-    time.sleep(2)
+        #go through one class
+        for course in courses:
+            course_dict = dict()
+            #loop though row of anchor tag
+            for child in course.contents:
+                #get inner HTML of row and split
+                parse=child.get_text().split("  ")
+                #add key,value pair of course data to dictionary
+                course_dict[parse[0]]=parse[1]
+            #add dictionary to course array
+            course_dict['name']=sel
+            course_array.append(course_dict)
 
-    soup=BeautifulSoup(table, 'html.parser')
+        #SAMPLE course_dict
+        #{'Class Nbr:': '38664', 'Section #:': '581', 'Type:': 'LEC', 'Campus:': 'CE', 'Meets:': 'Meets online', 
+        # 'Instructor:': 'D. Shaulis', 'Status:': 'Open', 'Dates:': '01-21 to 05-01'}
+        #
+        #{'Class Nbr:': '23529', 'Section #:': '005', 'Type:': 'LEC', 'Campus:': 'Main', 'Meets:': 'MTWF 1-1:50p',
+        #  'Instructor:': 'Hallowell/Lotfi', 'Status:': 'Open', 'Dates:': '01-13 to 04-30'}
 
+        #loop through course array add time and change meets format
+        for course in course_array:
+            #split on meets, you get days and the time
+            #split on - to get times
+            #append last letter of split string to first part
+            #now have start and end time
+            meets = course['Meets:'].split()
+            meet_days= meets[0]
+            meet_time =meets[1].split("-")
 
-    #anchor tags have all course info, is an array
-    courses = soup.find_all("a")
+            #splits date to be used in date time later
+            dates = course['Dates:'].split()
 
-    #array to hold course dictionaries
-    course_array = []
-
-    #go through one class
-    for course in courses:
-        course_dict = dict()
-        #loop though row of anchor tag
-        for child in course.contents:
-            #get inner HTML of row and split
-            parse=child.get_text().split("  ")
-            #add key,value pair of course data to dictionary
-            course_dict[parse[0]]=parse[1]
-        #add dictionary to course array
-        course_dict['name']=sel
-        course_array.append(course_dict)
-
-    #SAMPLE course_dict
-    #{'Class Nbr:': '38664', 'Section #:': '581', 'Type:': 'LEC', 'Campus:': 'CE', 'Meets:': 'Meets online', 
-    # 'Instructor:': 'D. Shaulis', 'Status:': 'Open', 'Dates:': '01-21 to 05-01'}
-    #
-    #{'Class Nbr:': '23529', 'Section #:': '005', 'Type:': 'LEC', 'Campus:': 'Main', 'Meets:': 'MTWF 1-1:50p',
-    #  'Instructor:': 'Hallowell/Lotfi', 'Status:': 'Open', 'Dates:': '01-13 to 04-30'}
-
-    #loop through course array add time and change meets format
-    for course in course_array:
-        #split on meets, you get days and the time
-        #split on - to get times
-        #append last letter of split string to first part
-        #now have start and end time
-        meets = course['Meets:'].split()
-        meet_days= meets[0]
-        meet_time =meets[1].split("-")
-
-
-
-        #splits date to be used in date time later
-        dates = course['Dates:'].split()
-
-        #online do not have to parse time
-        if(meet_time[0]=='online'):
-            meet_start='online'
-            meet_end='online'
-            #add start/end time to course dictionary
-            course['start_time']="online"
-            course['end_time']="online"
-        #parse time
-        else:
-            course['Meets:']=meet_days
-            meet_start = meet_time[0]
-            meet_end = meet_time[1]
-            #if start time is only one number add 0 before and colon and zeroes after
-            if(len(meet_start)<2):
-                am_pm= meet_end[-1]
-                meet_start= '0'+meet_start+':00'+am_pm
-            #if start time has 2 digits just add colon and zeroes
-            elif(len(meet_start)<3):
-                am_pm= meet_end[-1]
-                meet_start= meet_start+':00'+am_pm
-            #just add am/pm to start time
+            #online do not have to parse time
+            if(meet_time[0]=='online'):
+                meet_start='online'
+                meet_end='online'
+                #add start/end time to course dictionary
+                course['start_time']="online"
+                course['end_time']="online"
+            #parse time
             else:
-                am_pm= meet_end[-1]
-                meet_start= meet_start+am_pm
+                course['Meets:']=meet_days
+                meet_start = meet_time[0]
+                meet_end = meet_time[1]
+                #if start time is only one number add 0 before and colon and zeroes after
+                if(len(meet_start)<2):
+                    am_pm= meet_end[-1]
+                    meet_start= '0'+meet_start+':00'+am_pm
+                #if start time has 2 digits just add colon and zeroes
+                elif(len(meet_start)<3):
+                    am_pm= meet_end[-1]
+                    meet_start= meet_start+':00'+am_pm
+                #just add am/pm to start time
+                else:
+                    if(meet_start[-1]=='a' or meet_start[-1]=='p'):
+                        meet_start=meet_start
+                    else:
+                        am_pm= meet_end[-1]
+                        meet_start= meet_start+am_pm
 
-            #convert time to datetime object
-            start_time = parser.parse(dates[0]+" "+meet_start)
-            end_time = parser.parse(dates[2]+" "+meet_end)
+                #convert time to datetime object
+                start_time = parser.parse(dates[0]+" "+meet_start)
+                end_time = parser.parse(dates[2]+" "+meet_end)
 
-            #add start/end time to course dictionary
-            course['start_time']=start_time
-            course['end_time']=end_time
-            
-    course_list.append(course_array)
+                #add start/end time to course dictionary
+                course['start_time']=start_time
+                course['end_time']=end_time
+                
+        course_list.append(course_array)
 
 
 driver.close()
-pprint(course_list)
+
 
 
 #HAVE CLASS DATA
@@ -150,6 +178,22 @@ pprint(course_list)
 #
 #
 #
+#BRAINSTORM
+#first class selection determined by scarcity, display options for least dependent class then 
+#do this over and over again.
+class_file= open("test.txt", "w")
+for course in course_list:
+    class_name= course[0]['name']+':\n'
+    class_file.write(class_name)
+    for option in course:
+        class_string = str(option)
+        class_string= class_string+'\n'        
+        class_file.write(class_string)
+    class_file.write('\n')
+
+class_file.close()
+    
+    
 
 
 print("executed normally")
